6a04d4451d1b5024195a4c0934d83e3ed12e1b8d
Debojit <debsarkardebojit2006@gmail.com>
2026-07-07 22:37:43 +0530

    Add customer router

--- src/Routers/CustomerRouter.jsx
+++ src/Routers/CustomerRouter.jsx
@@ -1,33 +1,25 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import HomePage from '../customer/pages/homepage/homepage'
 import Cart from '../customer/components/cart/cart'
+import Product from '../customer/components/Product/Product'
 import ProductDetails from '../customer/components/ProductDetails/ProductDetails'
+import Checkout from '../customer/components/cart/CheckOut/checkOut'
+import Order from '../customer/components/Order/Order'
+import OrderDetails from '../customer/components/Order/OrderDeatils/OrderDetails'
 
 const CustomerRouter = () => {
   return (
-    <div>
-        <div>
-            
-                
-            </div>
-            <Routes>
-                <Route path='/' element={<HomePage/>}></Route>
-                <Route path='/cart' element={<Cart/>}></Route>
-                <Route path='/:levelOne/:levelTwo/:levelThree' element={<Product/>}></Route>
-                <Route path='/product/:productId' element={<ProductDetails/>}></Route>
-                <Route path='/checkout' element={<Checkout/>}></Route>
-                <Route path='/account/order' element={<Order/>}></Route>
-                <Route path='/account/order/:orderId' element={<OrderDetails/>}></Route>
-
-        
-            {/* <order/> */}
-            {/* <OrderDetails/> */}
-            </Routes>
-
-        <div>
-            <Footer/>
-        </div>
-    </div>
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+      <Route path='/cart' element={<Cart />} />
+      <Route path='/products/:levelOne/:levelTwo/:levelThree' element={<Product />} />
+      <Route path='/product/:productId' element={<ProductDetails />} />
+      <Route path='/checkout' element={<Checkout />} />
+      <Route path='/account/order' element={<Order />} />
+      <Route path='/account/order/:orderId' element={<OrderDetails />} />
+    </Routes>
   )
 }
 
-export default CustomerRouters
+export default CustomerRouter
