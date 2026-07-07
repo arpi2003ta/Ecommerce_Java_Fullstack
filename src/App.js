97bc13cce4394f709ee259a01d239184d8cb3cc5
Debojit <debsarkardebojit2006@gmail.com>
2026-07-07 22:39:36 +0530

    Update App.js routes and imports

--- src/App.js
+++ src/App.js
@@ -1,36 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
-import Navigation from './customer/components/Navigation/Navigation'
-import homepage from './customer/pages/homepage/homepage'
-import footer from './customer/components/footer/footer'
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout/Layout'
+import Homepage from './customer/pages/homepage/homepage'
 import Product from './customer/components/Product/Product'
 import ProductDetails from './customer/components/ProductDetails/ProductDetails'
-import CheckOut from './customer/components/CheckOut/'
-
-
+import Cart from './customer/components/cart/cart'
+import Checkout from './customer/components/cart/CheckOut/checkOut'
+import Order from './customer/components/Order/Order'
+import OrderDetails from './customer/components/Order/OrderDeatils/OrderDetails'
+import Login from './customer/components/Auth/Login'
+import Register from './customer/components/Auth/Register'
+import SearchResults from './customer/components/Search/SearchResults'
+import AdminDashboard from './customer/components/Admin/AdminDashboard/AdminDashboard'
+import ProtectedRoute from './components/Guards/ProtectedRoute'
+import AdminRoute from './components/Guards/AdminRoute'
 
 function App() {
   return (
-    <div className="">
-      <navigation />
-      <Routes>
-        <Route path='/*' element={<CustomerRouters />}><Route />
+    <Routes>
+      <Route element={<Layout />}>
+        {/* Public routes */}
+        <Route index element={<Homepage />} />
+        <Route path="products/:levelOne/:levelTwo/:levelThree/:levelFour" element={<Product />} />
+        <Route path="products/:levelOne/:levelTwo/:levelThree" element={<Product />} />
+        <Route path="product/:productId" element={<ProductDetails />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="products/search" element={<SearchResults />} />
 
-        <Routes/>
-        <div>
-          <homepage/>
-        </div>
-        <div>
-          {/* <homepage/> */}
-          {/* <Product/> */}
-          {/* <ProductDetails/> */}
-          {/* <cart/> */}
-          {/* <cCheckOut/> */}
-          {/* <order/> */}
-          {/* <OrderDetails/> */}
+        {/* Admin route */}
+        <Route path="admin" element={
+          <AdminRoute>
+            <AdminDashboard />
+          </AdminRoute>
+        } />
 
-        </div>
-    </div>
+        {/* Protected customer routes */}
+        <Route path="cart" element={
+          <ProtectedRoute>
+            <Cart />
+          </ProtectedRoute>
+        } />
+        <Route path="checkout" element={
+          <ProtectedRoute>
+            <Checkout />
+          </ProtectedRoute>
+        } />
+        <Route path="account/order" element={
+          <ProtectedRoute>
+            <Order />
+          </ProtectedRoute>
+        } />
+        <Route path="account/order/:orderId" element={
+          <ProtectedRoute>
+            <OrderDetails />
+          </ProtectedRoute>
+        } />
+      </Route>
+    </Routes>
   );
 }
 
